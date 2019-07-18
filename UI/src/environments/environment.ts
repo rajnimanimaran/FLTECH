85ebb2aee57f4a97112d51a9cea619c6e740d11d
rajinimanimaran <rajinimanimaran@gmail.com>
2019-07-18 14:23:36 +0530

    simple fix

--- UI/src/environments/environment.ts
+++ UI/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  apiUrl    : "http://localhost:61368/",
-// apiUrl    : "http://35.162.203.7/Flowtech-api/"
+//  apiUrl    : "http://localhost:61368/",
+ apiUrl    : "http://35.162.203.7/Flowtech-api/"
 };
 
 /*
